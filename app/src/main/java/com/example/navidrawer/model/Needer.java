060bbf2694c5d85e8179b11f3317eb7d193211e7
user <it20633622@my.sliit.lk>
2022-05-07 21:42:25 +0100

    Needer Registration Acceptance

--- app/src/main/java/com/example/navidrawer/model/Needer.java
+++ app/src/main/java/com/example/navidrawer/model/Needer.java
@@ -3,7 +3,7 @@ package com.example.navidrawer.model;
 import android.os.Parcel;
 import android.os.Parcelable;
 
-public class Needer implements Parcelable{
+public class Needer {
 
     private String RegistrationNumber;
     private String OrganizationName;
@@ -13,14 +13,11 @@ public class Needer implements Parcelable{
     private String Name;
     private String Designation;
     private String EmergencyContactNumber;
-    private String Password;
-    private String Accept;
-    private String NeederID;
 
     public Needer() {
     }
 
-    public Needer(String registrationNumber, String organizationName, String email, String contactNumber, String address, String name, String designation, String emergencyContactNumber, String password, String accept, String neederID) {
+    public Needer(String registrationNumber, String organizationName, String email, String contactNumber, String address, String name, String designation, String emergencyContactNumber) {
         RegistrationNumber = registrationNumber;
         OrganizationName = organizationName;
         Email = email;
@@ -29,9 +26,6 @@ public class Needer implements Parcelable{
         Name = name;
         Designation = designation;
         EmergencyContactNumber = emergencyContactNumber;
-        Password = password;
-        Accept = accept;
-        NeederID = neederID;
     }
 
     public String getRegistrationNumber() {
@@ -98,73 +92,4 @@ public class Needer implements Parcelable{
         EmergencyContactNumber = emergencyContactNumber;
     }
 
-    public String getPassword() {
-        return Password;
-    }
-
-    public void setPassword(String password) {
-        Password = password;
-    }
-
-    public String getAccept() {
-        return Accept;
-    }
-
-    public void setAccept(String accept) {
-        Accept = accept;
-    }
-
-    public String getNeederID() {
-        return NeederID;
-    }
-
-    public void setNeederID(String neederID) {
-        NeederID = neederID;
-    }
-
-    protected Needer(Parcel in) {
-        RegistrationNumber = in.readString();
-        OrganizationName = in.readString();
-        Email = in.readString();
-        ContactNumber = in.readString();
-        Address = in.readString();
-        Name = in.readString();
-        Designation = in.readString();
-        EmergencyContactNumber = in.readString();
-        Password = in.readString();
-        Accept = in.readString();
-        NeederID = in.readString();
-    }
-
-    public static final Creator<Needer> CREATOR = new Creator<Needer>() {
-        @Override
-        public Needer createFromParcel(Parcel in) {
-            return new Needer(in);
-        }
-
-        @Override
-        public Needer[] newArray(int size) {
-            return new Needer[size];
-        }
-    };
-
-    @Override
-    public int describeContents() {
-        return 0;
-    }
-
-    @Override
-    public void writeToParcel(Parcel parcel, int i) {
-        parcel.writeString(RegistrationNumber);
-        parcel.writeString(OrganizationName);
-        parcel.writeString(Email);
-        parcel.writeString(ContactNumber);
-        parcel.writeString(Address);
-        parcel.writeString(Name);
-        parcel.writeString(Designation);
-        parcel.writeString(EmergencyContactNumber);
-        parcel.writeString(Password);
-        parcel.writeString(Accept);
-        parcel.writeString(NeederID);
-    }
 }
