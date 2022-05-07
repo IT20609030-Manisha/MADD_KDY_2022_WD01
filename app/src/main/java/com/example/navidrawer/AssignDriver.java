c39260e8e2789ad14de78d311d7359d930089c0d
Nethmee <88671996+IT20622800@users.noreply.github.com>
2022-05-08 00:13:17 +0530

    Assign Driver xml & java files

---
+++ app/src/main/java/com/example/navidrawer/AssignDriver.java
@@ -0,0 +1,14 @@
+package com.example.navidrawer;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AssignDriver extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_assign_driver);
+    }
+}
